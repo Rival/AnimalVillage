c45d5bef757b06a18fecd5a72979f4f1b8299302
Andrey <COPATHuK@gmail.com>
2024-10-07 00:51:16 +0700

    [*] new version

--- Build/animal_village_WEBGL.framework.js
+++ Build/animal_village_WEBGL.framework.js
@@ -1994,13 +1994,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  5815152: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 5815213: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 5815277: function() {return Module.webglContextAttributes.powerPreference;},  
- 5815335: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 5815390: function($0) {performance.now = function() { return $0; };},  
- 5815438: function($0) {performance.now = function() { return $0; };},  
- 5815486: function() {performance.now = Module['emscripten_get_now_backup'];}
+  5815232: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 5815293: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 5815357: function() {return Module.webglContextAttributes.powerPreference;},  
+ 5815415: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 5815470: function($0) {performance.now = function() { return $0; };},  
+ 5815518: function($0) {performance.now = function() { return $0; };},  
+ 5815566: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
